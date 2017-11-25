7026548a8e3c09d2304719459a65bfc370340d63
abc010sdo <958993186@qq.com>
2017-11-25 14:43:43 +0800

    新增注释

--- src/com/taiji/function/StudentFactory.java
+++ src/com/taiji/function/StudentFactory.java
@@ -2,6 +2,24 @@ package com.taiji.function;
 
 import com.taiji.entity.Student;
 
+/**
+ * 
+ * @author abc010sdo
+ *
+ */
 public interface StudentFactory {
+
+	/**
+	 * 学生构建者
+	 * 
+	 * @param id
+	 *            学生编号
+	 * @param name
+	 *            学生姓名
+	 * @param score
+	 *            成绩
+	 * @return 学生
+	 */
 	Student build(int id, String name, int score);
+
 }
